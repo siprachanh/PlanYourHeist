993178c88df784daf259e34713f008d30beecd6b
Sippy Chanthaphaychith <sipra.chant@gmail.com>
2022-05-27 15:59:35 -0500

    printed successful and unsuccessful heists

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace PlanYourHeist
             Console.WriteLine("Plan Your Heist!");
             Console.WriteLine("-----------------");
 
+
+
+
             List<TeamMember> teamList = new List<TeamMember>();
 
 
@@ -88,34 +91,67 @@ namespace PlanYourHeist
                 TeamMember member = new TeamMember(name, skillLvl, courageFact);
 
             }
+            Random rnd = new Random();
+            int randomNum = rnd.Next(-10, 11);
+            Bank thisBank = new Bank(100 + randomNum);
+
+            Console.Write("Choose a bank difficulty:");
+            int bankDiff = 0;
+            string userBankDiff = Console.ReadLine();
+            bool success = int.TryParse(userBankDiff, out bankDiff);
+            if (success)
+            // will do this if true
+            {
+                thisBank.Difficulty = bankDiff;
+            }
+            else
+            {
+                while (!success)
+                {
+                    Console.WriteLine("Bank difficulty is number:");
+                    Console.Write("Choose a bank difficulty");
+
+                    userBankDiff = Console.ReadLine();
+                    success = int.TryParse(userBankDiff, out bankDiff);
+                }
+            }
+
+
+
             newTeam.Description();
             Console.WriteLine("How many times do you want to run this?");
             string runTrial = Console.ReadLine();
             int trialRun = int.Parse(runTrial);
+            int successReport = 0;
+            int failedReport = 0;
 
             int i = 0;
             while (i < trialRun)
             {
-                Random rnd = new Random();
-                int randomNum = rnd.Next(-10, 11);
+                Random random = new Random();
+                int randomNumber = rnd.Next(-10, 11);
                 // generate min/max int
 
 
-                Bank thisBank = new Bank(100 + randomNum);
+                Bank thisBank1 = new Bank(bankDiff + randomNumber);
                 Console.WriteLine($"Combined skill level is: {skillSum}");
-                Console.WriteLine($"The bank's difficulty level is: {thisBank.Difficulty}");
+                Console.WriteLine($"The bank's difficulty level is: {thisBank1.Difficulty}");
 
-                if (skillSum >= thisBank.Difficulty)
+                if (skillSum >= thisBank1.Difficulty)
                 {
 
                     Console.WriteLine("You robbed the bank!");
+                    successReport++;
                 }
                 else
                 {
                     Console.WriteLine("Try again next time.");
+                    failedReport++;
                 }
                 i++;
+
             }
+            Console.WriteLine($"Your team had {successReport} successful runs and {failedReport} unsuccessful runs.");
         }
     }
 }
